50037c3957afee7e7a46d51c1914cfb9efb62b2a
javier maita <javiermaita@hotmail.com.ar>
2022-09-06 20:27:58 -0300

    2doDesafio

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './componentes/Header/Header';
-import Main from './componentes/Main/Main';
-import Footer from './componentes/Footer/Footer';
 
 function App() {
   return (
     <>
       <Header/>
-      <Main/>
-      <Footer/>
     </>
   );
 }
--- src/componentes/Header/Header.js
+++ src/componentes/Header/Header.js
@@ -1,20 +1,26 @@
 import React from "react";
 import Icono from "./icono";
 import Carrito from "./Carrito";
-
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 import "./Header.css";
 
 function Header() {
-    return(
-        <div className="NavBar">
-            <Icono/>
-            <div className="NavsConteiner">
-                <a className="enlase">Nosotros</a>
-                <a className="enlase">Deseados</a>
-                <a className="enlase carrito"><Carrito/></a>
-            </div>
-        </div>
-    );
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container className="NavBar-Conteiner">
+        <Navbar.Brand href="#home" className="Logo"><Icono/></Navbar.Brand>
+        <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto links">
+                <Nav.Link href="#home" className="enlase">Nosotros</Nav.Link>
+                <Nav.Link href="#link" className="enlase">Deseados</Nav.Link>
+                <Nav.Link href="#link" className="Carrito"><Carrito/></Nav.Link>
+            </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default Header;
